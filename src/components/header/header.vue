20d3d4e644c51d9ce228331b4e61da7048c373af
Hcki186 <hackicrew186@gmail.com>
2023-09-04 14:36:18 +0200

    Vue Component header and scss

---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="headerBar container">
+        <ul class="bar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#rental">Rental Car</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about-us">About Us</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderBar"
+    }
+</script>
