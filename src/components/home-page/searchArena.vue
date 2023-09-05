1b58a4acf7b62ec175164192811905245e9275c5
Hcki186 <hackicrew186@gmail.com>
2023-09-05 12:24:52 +0200

    creating search form

---
+++ src/components/home-page/searchArena.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="searchForm container">
+        <div class="form">
+            <select name="manufacturer" id="manufacturer">
+                <option value="brand">Značka</option>
+                <option value="audi">Audi</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="bmw">BMW</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchForm"
+}
+</script>
