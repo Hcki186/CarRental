8236edcb11107a887c12b343db5d6cd1af135465
Hcki186 <hackicrew186@gmail.com>
2023-09-05 11:04:59 +0200

    Home page text and background

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="HomePage">
+      <HeaderBar></HeaderBar>
+      <HomePageText></HomePageText>
+      <SearchForm></SearchForm>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBar from "./components/header/header.vue"
+import HomePageText from "./components/home-page/homePage.vue"
+import SearchForm from "./components/home-page/searchArena.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBar,
+    HomePageText,
+    SearchForm
   }
 }
 </script>
@@ -21,8 +28,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/home-page/homePage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="homePageText container">
+        <h1>POŽIČAJTE SI AUTO</h1>
+        <p>Rýchlo. Jednoducho. Výhodne.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomePageText"
+}
+</script>
