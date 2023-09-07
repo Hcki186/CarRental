e0174182234a60d508649074e857ebff15151f9c
Hcki186 <hackicrew186@gmail.com>
2023-09-07 12:41:09 +0200

    adding button in search form

---
+++ src/components/button/searchbutton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="searchButton">
+        <h1>Vvyhľadať</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "searchButton"
+}
+</script>
--- src/components/home-page/searchArena.vue
+++ src/components/home-page/searchArena.vue
@@ -1,18 +1,34 @@
 <template>
     <div class="searchForm container">
         <div class="form">
-            <select name="manufacturer" id="manufacturer">
+            <select name="manufacturer" id="manufacturer" class="dropDown">
                 <option value="brand">Značka</option>
                 <option value="audi">Audi</option>
                 <option value="mercedes">Mercedes</option>
                 <option value="bmw">BMW</option>
             </select>
+            <select name="category" id="category" class="dropDown">
+                <option value="class">Trieda</option>
+                <option value="economic">ekonomicka</option>
+                <option value="medium">stredna</option>
+                <option value="luxurious">luxusna</option>
+            </select>
+            <searchbutton></searchbutton>
         </div>
     </div>
 </template>
 
+
 <script>
+
+import searchButton from "@/components/button/searchbutton.vue" 
+import searchbutton from '@/components/button/searchbutton.vue'
+
 export default {
-    name: "SearchForm"
+  components: { searchbutton },
+    name: "SearchForm",
+    component: {
+        searchButton
+    }
 }
 </script>
