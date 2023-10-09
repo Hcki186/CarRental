9a615450c2bcf7eb39dacb863f88a2416741b04e
Hcki186 <hackicrew186@gmail.com>
2023-10-09 13:20:04 +0200

    Home page updating

---
+++ src/components/home-page/categoryCart.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class=" category">
+        <div class="">
+            <h1>Ponuka vozidiel</h1>
+        </div>
+        <div class="categoryCart container">
+            <Div class="cards">
+                <div class="economiCard card">
+                    <p>Ekonomická</p>
+                </div>
+                <div class="mediumCard card">
+                    <p>Stredná</p>
+                </div>
+                <div class="luxuryCard card">
+                    <p>Luxusná</p>
+                </div>
+            </Div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "categoryCart"
+}
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="HomePage ">
+      <HomePageText></HomePageText>
+      <categoryCart></categoryCart> 
+    </div>
+</template>
+
+<script>
+import HomePageText from "@/components/home-page/homePage.vue"
+import categoryCart from "@/components/home-page/categoryCart.vue"
+
+
+export default {
+  name: 'HomePage',
+  components: {
+    HomePageText,
+    categoryCart
+  }
+
+}
+</script>
+
+<style>
+
+</style>
