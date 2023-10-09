bf5a9b6cbfa3bcdbd1384adae16b5ac4c0e48aa0
Hcki186 <hackicrew186@gmail.com>
2023-10-09 13:13:53 +0200

    About us componet

---
+++ src/components/aboutUs/aboutUs.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="textarea">
+        <div class="container">
+            <h1>O nás</h1>
+            <p>Sme frima, ktorá na trhu aktívne figuruje už od roku 2006. Vďaka dlhoročnému pôsobeniu firmy mame dostatočne skusenosti
+                a poskytujeme profesionalne služby, invidualni pristup k našim klientom a ich komfort. Aj preto mame pripravené vozidla na 
+                sprevádzať Vaše cesty odkiaľkoľvek.Ponúkame vozidlá z kategórií ekonomická trieda, stredná trieda, luxusné vozidlá.</p>
+        </div>
+        <div class="CarBG"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AboutUsText"
+}
+</script>
