bd58043d47905fa56ff234cf2a9d0c74c8f0863c
Hcki186 <hackicrew186@gmail.com>
2023-10-09 13:16:53 +0200

    Make provisional footer

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="footerContact">
+        <div class="content container">
+            <div class="information">
+                <ul class="company">
+                    <h1>Company</h1>
+                    <li><router-link to="/">Home</router-link></li>
+                    <li><router-link to="/AboutUs">About Us</router-link></li>
+                    <li><router-link to="/RentalCar">Rental Car</router-link></li>
+                </ul>
+                <ul id="contact">
+                    <h1>Contact</h1>
+                    <li><a href="mailto: company@gmail.com">company@gmail.com</a></li>
+                    <li><a href="tel: 0915718986">0915718986</a></li>
+                </ul>
+            </div>
+            <div class="rights">
+                <p>All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FooterContact"
+    }
+</script>
