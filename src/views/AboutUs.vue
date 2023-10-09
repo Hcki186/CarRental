029d41c99119b7126fe010a81ae935357fcf0ae7
Hcki186 <hackicrew186@gmail.com>
2023-10-09 13:13:05 +0200

    Make About us slide

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="AboutUs">
+        <AboutUsText></AboutUsText>
+    </div>
+</template>
+
+<script>
+
+import AboutUsText from "@/components/aboutUs/aboutUs.vue"
+
+
+export default {
+  name: 'AboutUs',
+  components: {
+    AboutUsText
+  }
+
+}
+</script>
