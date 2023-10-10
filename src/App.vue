46669797c46594aee03da6b138ef24eae39510da
Hcki186 <hackicrew186@gmail.com>
2023-10-10 10:29:18 +0200

    adding router to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
   <div id="app">
-    <div class="HomePage">
-      <HeaderBar></HeaderBar>
-      <HomePageText></HomePageText>
-      <SearchForm></SearchForm>
-    </div>
+    <HeaderBar></HeaderBar>
+    <router-view  />
+    <FooterContact></FooterContact>
   </div>
 </template>
 
 <script>
-import HeaderBar from "./components/header/header.vue"
-import HomePageText from "./components/home-page/homePage.vue"
-import SearchForm from "./components/home-page/searchArena.vue"
+import HeaderBar from "./components/header/header.vue";
+import FooterContact from "./components/footer/footer.vue"
 
 export default {
   name: 'App',
   components: {
     HeaderBar,
-    HomePageText,
-    SearchForm
+    FooterContact
+  
   }
 }
 </script>
@@ -28,5 +25,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  position: relative;
+  min-height: 100vh;
 }
+
+
+
+
+
 </style>
