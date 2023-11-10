01b071dd903e663b73ae7f63ef1531e5ab4c5167
Hcki186 <hackicrew186@gmail.com>
2023-11-10 12:58:09 +0100

    create fetail car view slide

---
+++ src/views/DetailCar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div>
+      <P>TEST TEST TEST TEST {{ $route.params.id }} </P>
+      <p>{{ $route.params.id }}</p>
+      <p>{{ $props.id }}</p>
+
+      <p>
+        {{ wantedData.name }}
+      </p>
+
+
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import itemData from "../../Api.json"
+
+
+export default {
+    name: "DetailCar",
+    data() {
+        return {
+          items: itemData,
+          data: []
+        }
+    }, 
+    props: {
+    id: {
+        type: String,
+        default: "0",
+      },
+    },
+    mounted() {
+
+      this.log()
+
+    },
+
+    computed: {
+      wantedData() {
+          return this.items.find((i) =>{
+            return i.id == this.$router.currentRoute.params.id 
+          })
+      }
+      
+    },
+    methods: {
+
+
+      log(){
+        console.log()
+      }, 
+              
+      
+    }
+}
+</script>
+
+<style>
+
+</style>
